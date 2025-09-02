76f6481dbc8e1a7784a4b3c567275e075db3b039
nachofazah <ignaciomartin.fazahbeiroa@estudiantes.unahur.edu.ar>
2025-09-02 16:48:02 -0300

    reformas responsive

--- funciones.js
+++ funciones.js
@@ -1,6 +1,7 @@
 const boton = document.getElementById("modo")
 const info = document.querySelector(".info")
 const info2 = document.querySelectorAll(".info2")
+const info3 = document.querySelectorAll(".info3")
 const caja = document.querySelectorAll(".caja")
 const header = document.querySelector("header")
 const container = document.getElementById("container")
@@ -10,6 +11,7 @@ const titulos2 = document.querySelectorAll("h4")
 const registro = document.getElementById("registro")
 const instagram = document.getElementById("instagram")
 const linkedin = document.getElementById("linkedin")
+const boton2 = document.getElementById("btnRegistro");
 
 registro.setAttribute("href", "register.html")
 instagram.setAttribute("href", "https://www.instagram.com/nachofzh/")
@@ -17,7 +19,7 @@ linkedin.setAttribute("href", "https://www.linkedin.com/in/ignacio-martin-fazah-
 
 
     
-  boton.addEventListener("click", function(){
+boton.addEventListener("click", function(){
     info.classList.toggle("dark")
     header.classList.toggle("darkB")
     container.classList.toggle("dark")
@@ -32,6 +34,10 @@ linkedin.setAttribute("href", "https://www.linkedin.com/in/ignacio-martin-fazah-
         info2[i].classList.toggle("dark")
     }    
 
+    for(var i = 0; i < info2.length; i++){
+        info3[i].classList.toggle("dark")
+    }    
+
     for(var i = 0; i < caja.length; i++){
         caja[i].classList.toggle("dark")
     }
@@ -40,3 +46,9 @@ linkedin.setAttribute("href", "https://www.linkedin.com/in/ignacio-martin-fazah-
         titulos[i].classList.toggle("dark")
     } 
 })
+
+
+boton2.addEventListener("click", () => {
+// redirige a la página de registro
+    window.location.href = "register.html";
+});
